ba898e91f9a353a8d5d7bdd8db380e1bbc72f52d
francis kamande <franciskamande2001@gmail.com>
2022-03-22 20:30:07 +0300

    linkedlists in C

---
+++ linkedlistsintro/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+struct Node* head;
+void insert(int x)
+{
+    struct Node* temp = (Node*)malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->next=head;
+    head =temp;
+}
+void print()
+{
+    struct Node* temp = head;
+    printf("List is: ");
+    while(temp != NULL){
+        printf("%d",temp->data);
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    int i, n, m;
+    printf("How many values in the linked list:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+       printf("Enter number %d: ",i+1);
+       scanf("%d",&m);
+       insert(m);
+       print();
+    }
+    return 0;
+}
